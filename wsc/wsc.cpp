9beb6532f09950c756c983db3b7debfce1237b5a
jingjiajie <932166095@qq.com>
2019-03-24 23:55:57 +0800

    tmp

--- wsc/wsc.cpp
+++ wsc/wsc.cpp
@@ -6,15 +6,26 @@
 #include "WorkScriptCompiler.h"
 #include "CmdArg.h"
 
+#include <llvm/ADT/Triple.h>
+#include <llvm/Support/Host.h>
+
 using namespace std;
 using namespace WorkScript;
 
 int main(int argc, const char **argv) {
 	setlocale(LC_CTYPE,"");
-	CmdArgs args;
-	args.parse(argc, argv);
-	auto fileNames = args.getRestArgs();
-	WorkScriptCompiler compiler;
-	CompileResult ret = compiler.compile(fileNames);
-	return (int)ret;
+
+	auto tp = llvm::sys::getDefaultTargetTriple();
+	auto cpu = llvm::sys::getHostCPUName();
+	auto pt = llvm::sys::getProcessTriple();
+	llvm::Triple triple("mips123-pc-linux-gnu");
+	llvm::Triple::ArchType ::
+	return 0;
+
+//	CmdArgs args;
+//	args.parse(argc, argv);
+//	auto fileNames = args.getRestArgs();
+//	WorkScriptCompiler compiler;
+//	CompileResult ret = compiler.compile(fileNames);
+//	return (int)ret;
 }
